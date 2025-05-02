89c8c897ec4f5baa2c3e3b962ea0cda756072094
D-Kamunya <kinc.developer@gmail.com>
2025-05-02 15:51:37 +0300

    ch:services stash

---
+++ src/sections/Services/ServiceData.js
@@ -0,0 +1,49 @@
+import iconOne from '../../assets/service/1.png';
+import iconTwo from '../../assets/service/2.png';
+import iconThree from '../../assets/service/3.png';
+import iconFour from '../../assets/service/4.png';
+import iconFive from '../../assets/service/5.png';
+const ServicesData = [
+    {
+        'icon': iconOne,
+        'title': 'Consultation',
+        'description': 'Adults 1000',
+    },
+    {
+        'icon': iconFive,
+        'title': 'Clinicals',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    },
+    {
+        'icon': iconOne,
+        'title': 'X-Rays',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    },
+    {
+        'icon': iconOne,
+        'title': 'Reconstruction Procedures',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    },
+    {
+        'icon': iconFour,
+        'title': 'Endodontics',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    },
+    {
+        'icon': iconOne,
+        'title': 'Restorative Dentistry(Replacement)',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    },
+    {
+        'icon': iconOne,
+        'title': 'Permanent Replacement',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    },
+    {
+        'icon': iconThree,
+        'title': 'Cosmetic Dentistry',
+        'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
+    }
+]
+
+export default ServicesData;
