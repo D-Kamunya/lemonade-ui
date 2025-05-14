89151362ec5ccc57c62cfaa8b753266e7a2131e3
D-Kamunya <kinc.developer@gmail.com>
2025-05-14 11:27:38 +0300

    ch: create service section

--- src/sections/Services/ServiceData.js
+++ src/sections/Services/ServiceData.js
@@ -3,6 +3,9 @@ import iconTwo from '../../assets/service/2.png';
 import iconThree from '../../assets/service/3.png';
 import iconFour from '../../assets/service/4.png';
 import iconFive from '../../assets/service/5.png';
+import iconSix from '../../assets/service/6.png';
+import iconSeven from '../../assets/service/7.png';
+import iconEight from '../../assets/service/8.png';
 const ServicesData = [
     {
         'icon': iconOne,
@@ -10,37 +13,37 @@ const ServicesData = [
         'description': 'Adults 1000',
     },
     {
-        'icon': iconFive,
+        'icon': iconTwo,
         'title': 'Clinicals',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     },
     {
-        'icon': iconOne,
+        'icon': iconThree,
         'title': 'X-Rays',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     },
     {
-        'icon': iconOne,
+        'icon': iconFour,
         'title': 'Reconstruction Procedures',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     },
     {
-        'icon': iconFour,
+        'icon': iconFive,
         'title': 'Endodontics',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     },
     {
-        'icon': iconOne,
+        'icon': iconSix,
         'title': 'Restorative Dentistry(Replacement)',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     },
     {
-        'icon': iconOne,
+        'icon': iconSeven,
         'title': 'Permanent Replacement',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     },
     {
-        'icon': iconThree,
+        'icon': iconEight,
         'title': 'Cosmetic Dentistry',
         'description': 'Lorem ipsum is placeholder text commonly used in the graphic,',
     }
