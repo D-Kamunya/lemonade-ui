157553b69e77cb463a601b4a1560854b37b1bbf5
D-Kamunya <kinc.developer@gmail.com>
2025-05-16 10:13:30 +0300

    ft: Add responsive layout for mobile and large screens

--- src/sections/Testimonial/Testimonial.js
+++ src/sections/Testimonial/Testimonial.js
@@ -43,7 +43,30 @@ const Testimonial = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     };
 
     return (
